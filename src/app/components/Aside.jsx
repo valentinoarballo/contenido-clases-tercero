20711d46e6cdecbc75589092b5ccefa639064dca
valentinoarballo <v.arballo@itecriocuarto.org.ar>
2026-05-15 00:04:48 -0300

    DOM y eventos edicion

--- src/app/components/Aside.jsx
+++ src/app/components/Aside.jsx
@@ -1,6 +1,66 @@
+"use client"
+import { useState, useEffect } from 'react';
+import { useNotes } from '../notes/NotesContext'
+
 import Link from "next/link"
 
+function CategorySection({ category }) {
+
+  const [isOpen, setIsOpen] = useState(true)
+
+  return (
+    <div>
+
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className='flex cursor-pointer items-center gap-2'
+      >
+        <h1>
+          {category.title}
+        </h1>
+        <svg width="16" height="16" fill="currentColor" className={`bi bi-caret-down-fill ${isOpen ? "rotate-180" : "rotate-0"}`} viewBox="0 0 16 16">
+          <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+        </svg>
+      </button>
+
+
+      <div className={`grid transition-all duration-300 ease-in-out ${isOpen ? "flex" : "hidden"}`}>
+        <ul>
+          {category.notes.map((subItem, index) => (
+            <li key={index} className="pl-4 cursor-pointer transition-all duration-200 hover:scale-105">
+              <Link href={`/notes/${subItem.id}`}> {subItem.title} </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 export default function Aside({ data }) {
+
+  const { addCategories } = useNotes()
+  const [newCat, setNewCat] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const handleAddCategory = (e) => {
+    e.preventDefault()
+    if (!newCat) return;
+    addCategories(newCat)
+  }
+
+  const filteredData = data.map(category => {
+    const filteredNotes = category.notes.filter(nota =>
+      nota.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      nota.content.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return { ...category, notes: [filteredNotes] }
+  }).filter(category => {
+    category.notes.length > 0 ||
+      category.title.toLowerCase().includes(searchQuery.toLowerCase())
+  });
+
+  console.log(filteredData)
   return (
     <aside className="w-72 py-8 px-6 border-r border-zinc-700">
       <div>
@@ -8,20 +68,16 @@ export default function Aside({ data }) {
         <p>Todas nuestras notas</p>
       </div>
 
-      <div className="space-y-6 mt-8">
-        {data.map((item, index) => (
-          <div className="border-b pb-6 border-zinc-700" key={index}>
-            <h3 className="text-2xl font-semibold">
-              {item.title}
-            </h3>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
 
-            <ul>
-              {item.notes.map((subItem, index) => (
-                <li key={index} className="pl-4 cursor-pointer transition-all duration-200 hover:scale-105">
-                  <Link href={`/notes/${subItem.id}`}> {subItem.title} </Link>
-                </li>
-              ))}
-            </ul>
+      <div className="space-y-6 mt-8">
+        {filteredData.map((item, index) => (
+          <div className="border-b pb-6 border-zinc-700" key={index}>
+            <CategorySection category={item} />
           </div>
         ))}
       </div>
--- src/app/components/NoteDetail.jsx
+++ src/app/components/NoteDetail.jsx
@@ -1,9 +1,29 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNotes } from '../notes/NotesContext'
 
 export default function NoteDetail({ note }) {
-
+  const { updateNote } = useNotes()
   const [copied, setCopied] = useState(false);
+
+  const [editingFiled, setEditingField] = useState(null) // "title" o "content" o "ejemplo" o "null"
+
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    ejemplo: ""
+  });
+
+  useEffect(() => {
+    if (note) {
+      setFormData({
+        title: note.title,
+        content: note.content,
+        ejemplo: note.ejemplo || "",
+      })
+    }
+  }, [note])
+
 
   const handleCopy = () => {
     if (note.ejemplo) {
@@ -11,15 +31,44 @@ export default function NoteDetail({ note }) {
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     }
-
   };
+
+  const handleSave = () => {
+    updateNote(note.id, formData)
+    setEditingField(null)
+  }
+
+  const EditPencil = () => (
+    <svg width="16" height="16" fill="currentColor" className="bi bi-pencil opacity-0 group-hover:opacity-100" viewBox="0 0 16 16">
+      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+    </svg>
+  )
 
   return (
     <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          {note.title}
-        </h1>
+        <div
+          className='w-full hover:bg-zinc-800 transition-colors cursor-pointer rounded p-3 group'
+          onDoubleClick={() => setEditingField("title")}
+          title="Doble click para editar"
+        >
+          {editingFiled === "title" ? (
+            <input
+            type="text"
+            value={formData.title}
+            onChange={(e) => setFormData({...formData, title: e.target.value})}
+            autoFocus
+            className="max-w-xs text-3xl focus:outline-none focus:ring-2 focus:ring-green-300 w-full p-3 rounded m-1 font-semibold leading-10 tracking-tight text-black dark:text-zinc-50"
+            />
+          ) : (
+            <div className='flex items-center gap-2'>
+              <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                {note.title}
+              </h1>
+              <EditPencil />
+            </div>
+          )}
+        </div>
       </div>
 
       <p className='text-justify'>
@@ -56,6 +105,15 @@ export default function NoteDetail({ note }) {
           </pre>
         </div>
       </section>
+
+      {editingFiled && (
+        <button
+          onClick={handleSave}
+          className='bg-green-600 hover:bg-green-500 px-3 py-2 rounded cursor-pointer text-white'
+        >
+          Save
+        </button>
+      )}
 
     </main>
   )
