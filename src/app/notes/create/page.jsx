e32bd6ea329aa8ec955547af64888666b5d92b4c
valentinoarballo <v.arballo@itecriocuarto.org.ar>
2026-09-03 23:07:32 -0300

    validations con zod y manejo de formulario con rhf

--- src/app/notes/create/page.jsx
+++ src/app/notes/create/page.jsx
@@ -2,6 +2,9 @@
 import { useState } from 'react'
 import { useNotes } from '../NotesContext'
 import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { notesSchema } from '@/app/validations/NotesSchema'
 
 import Link from 'next/link'
 import React from 'react'
@@ -9,32 +12,36 @@ import React from 'react'
 function CreateNotePage() {
   const router = useRouter()
 
-
-
   const { addNote, getDynamicCategories } = useNotes()
 
   const categories = getDynamicCategories()
 
+<<<<<<< Updated upstream
   const [formData, setFormData] = useState({
     title: "",
     content: "",
     ejemplo: "",
     categoryId: 1
+=======
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(notesSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      ejemplo: "",
+      categoryId: "",
+    }
+>>>>>>> Stashed changes
   })
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    if (!formData.title || !formData.content) return alert("Title and content are required")
-
-    addNote(formData)
+  const onSubmit = (data) => {
+    addNote(data)
     router.push("/notes")
   }
 
-
   return (
     <section className='flex p-20 justify-center items-center w-full'>
-      <form className="flex flex-col flex-1  p-6 rounded-lg bg-zinc-800 font-sans">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col flex-1  p-6 rounded-lg bg-zinc-800 font-sans">
 
         <Link href={"/notes"} className="self-start mb-4 text-white font-semibold">
           &larr; Back to Notes
@@ -44,13 +51,14 @@ function CreateNotePage() {
         <div className='mt-10 flex flex-col gap-3'>
 
           <div className='flex flex-col'>
-            <label className='text-zinc-400'>Title</label>
+            <p className='text-zinc-400 text-lg'>Title <label className='text-sm text-red-800 animate-pulse'>{errors.title && "*" + errors.title.message}</label></p>
             <input
               type="text"
               placeholder='Title'
               className='p-2 border border-zinc-600 rounded-md my-4'
-              value={formData.title}
-              onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+              // value={formData.title}
+              // onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+              {...register("title")}
             />
           </div>
 
@@ -58,9 +66,15 @@ function CreateNotePage() {
             <label className='text-zinc-400'>Category</label>
             <select
               className='cursor-pointer p-2 border border-zinc-600 rounded-md my-4'
-              value={formData.categoryId}
-              onChange={(e) => setFormData({ ...formData, categoryId: String(e.target.value) })}
+              // value={formData.categoryId}
+              // onChange={(e) => setFormData({ ...formData, categoryId: String(e.target.value) })}
+              {...register("categoryId")}
             >
+<<<<<<< Updated upstream
+=======
+              <option disabled={true} >Select Category</option>
+
+>>>>>>> Stashed changes
               {categories.map((category) => (
                 <option key={category.id} value={category.id}>{category.title}</option>
               ))}
@@ -68,13 +82,14 @@ function CreateNotePage() {
           </div>
 
           <div className='flex flex-col'>
-            <label className='text-zinc-400'>Content</label>
+            <p className='text-zinc-400'>Content <label className='text-sm text-red-800 animate-pulse'>{errors.content && "*" + errors.content.message}</label> </p>
             <textarea
               placeholder='Content'
               className='p-2 border border-zinc-600 rounded-md my-4'
               rows={10}
-              value={formData.content}
-              onChange={(e) => setFormData({ ...formData, content: e.target.value })}
+              // value={formData.content}
+              // onChange={(e) => setFormData({ ...formData, content: e.target.value })}
+              {...register("content")}
             />
           </div>
 
@@ -84,12 +99,13 @@ function CreateNotePage() {
               placeholder='Const variable = ....'
               className='p-2 border border-zinc-600 rounded-md my-4'
               rows={10}
-              value={formData.ejemplo}
-              onChange={(e) => setFormData({ ...formData, ejemplo: e.target.value })}
+              // value={formData.ejemplo}
+              // onChange={(e) => setFormData({ ...formData, ejemplo: e.target.value })}
+              {...register("ejemplo")}
             />
           </div>
 
-          <button onClick={handleSubmit} className='bg-blue-500 text-white p-2 rounded-md'>Save</button>
+          <button type='submit' className='bg-blue-500 text-white p-2 rounded-md'>Save</button>
         </div>
       </form>
 
