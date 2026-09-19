dd5d09362044e8e9e6bcbaccb6e01d405f5efae6
valentinoarballo <v.arballo@itecriocuarto.org.ar>
2026-09-18 21:41:47 -0300

    test con vitest

---
+++ src/app/__tests__/NotesContext.test.jsx
@@ -0,0 +1,45 @@
+import { renderHook, act, waitFor } from "@testing-library/react";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { NotesProvider, useNotes } from "../context/NotesContext";
+import axios from "axios";
+
+
+vi.mock("axios")
+
+const wrapper = ({ children }) => <NotesProvider> { children } </NotesProvider>
+
+describe("Pruebas del custom hook: useNotes", () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks()
+
+        axios.get.mockImplementation((url) => {
+
+            if (url == "/api/notes") {
+                return Promise.resolve({ data: [{id: 1, title: "Nota inicial", content: "El contenido de mi nota", ejemplo: "let x", categoryId: "cat_1"}]})
+            }        
+
+            if (url == "/api/categories") {
+                return Promise.resolve({ data: [{id: 1, title: "Categoria inicial"}] })
+            }
+
+            return Promise.resolve({error: "404 Ruta no encontrada"})
+
+        })
+    })
+
+    it("debe cargar los datos iniciales de notas y categorias", async () => {
+        
+        const { result } = renderHook(() => useNotes(), { wrapper })
+        
+        await waitFor(() => {
+            expect(result.current.notes).toHaveLength(1)
+            expect(result.current.categories).toHaveLength(1)
+        })
+        
+        expect(result.current.notes[0].title).toBe("Nota inicial")
+
+    })
+
+})
+
